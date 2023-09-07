df53951041a74f2f87ad7fc2c4049f274a3e43de
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2023-09-07 15:06:32 -0500

    Image upload v2

--- client/src/componenets/ImageUpload.js
+++ client/src/componenets/ImageUpload.js
@@ -16,14 +16,30 @@ function ImageUpload() {
   function handleSubmit(e){
     e.preventDefault();
 
+    const formData = new FormData();
+      formData.append('image', image);
+      formData.append('caption', caption);
     
-  }
-
-  
+    fetch('/image_upload', {
+      method: 'POST',
+      body: formData,
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.message){
+        console.log('Image uploaded successfully.');
+      } else {
+        console.error('Error from server:', data.message);
+      }
+    })
+    .catch(err => {
+      console.error('There was an error uploading the image:', err);
+    })
+  };
   return (
     <form onSubmit={handleSubmit}>
       <input type="file" accept="image" onChange={handleImageChange}/>
-      <input type="text" placeholder='"Add a caption' value={caption} onChange={handleCaptionChange}/>
+      <input type="text" placeholder='Add a caption' value={caption} onChange={handleCaptionChange}/>
       <button type="submit">Upload</button>
     </form>
   )
