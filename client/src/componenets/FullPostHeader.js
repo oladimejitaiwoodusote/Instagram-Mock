3dd6c60837385cba92e23f667deab45eee53d22e
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2023-11-14 17:48:38 -0600

    Full Post refactor v2

--- client/src/componenets/FullPostHeader.js
+++ client/src/componenets/FullPostHeader.js
@@ -5,18 +5,18 @@ import './FullPostHeader.css'
 
 function FullPostHeader({user, post, onOptionsClick, showOptions, onEditClick, onDeleteClick}) {
   return (
-    <div className="FullPost_header">
-        <div className="FullPost_user_info">
+    <div className="FullPostHeader">
+        <div className="FullPostHeader_user_info">
             <Avatar src={post.avatar} alt={post.username}/>
             <Link to ={`/profile_page/${post.user_id}`} style={{textDecoration:'none', color: 'inherit'}}>                    
                 <p>{post.username}</p>
             </Link>
         </div>
         {post.user_id === user.id && (
-            <div className="FullPost_options">
-                <button className="FullPost_options_button" onClick={onOptionsClick}>...</button>
+            <div className="FullPostHeader_options">
+                <button className="FullPostHeader_options_button" onClick={onOptionsClick}>...</button>
                 {showOptions && (
-                    <div className='FullPost_options_menu'>
+                    <div className='FullPostHeader_options_menu'>
                         <button className='FullPost_edit_button' onClick={onEditClick}>Edit</button>
                         <button className='FullPost_delete_button' onClick={onDeleteClick}>Delete</button>
                     </div>
